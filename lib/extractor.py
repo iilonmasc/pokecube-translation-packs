df26bc0a3d3c49daa83fddc091c8ad878b1e2a8b
Tim Otlik <t.otlik@vivaldi.net>
2021-01-11 13:51:33 +0100

    feature: add jar extractor
      takes version info from MANIFEST.MF using:
        - either Implementation-Version
        - or Specification-Version
    
      takes original json files from mobs and moves from the jar instead of
      relying on manually extracted files put somewhere in the repository

--- lib/extractor.py
+++ lib/extractor.py
@@ -1,4 +1,126 @@
+import zipfile
+import os
+import json
 
 class BaseFileExtractor():
-    def __init__(self):
-        pass
+    def __init__(self, archive, debug=False):
+        self.archive_path = archive
+        self.archive = zipfile.ZipFile(self.archive_path)
+        self.debug = debug
+        self.asset_files = {
+            'base_mobs.json': 'assets/pokecube_mobs/lang/en_us.json',
+            'base_moves.json': 'assets/pokecube_moves/lang/en_us.json',
+        }
+
+    def extract_base_files(self):
+        for asset_file in self.asset_files:
+            self.extract_base_file(self.asset_files.get(asset_file), asset_file)
+
+    def clear_base_move_file(self):
+        ignore_fields=[
+    "_comment",
+    "pokemob.status.curse.user",
+    "pokemob.status.flinch.user",
+    "pokemob.status.confuse.add.user",
+    "pokemob.status.confuse.remove.user",
+    "pokemob.status.confusion.user",
+    "pokemob.status.infatuate.user",
+    "pokemob.move.stat.fail.user",
+    "pokemob.status.curse.target",
+    "pokemob.status.flinch.target",
+    "pokemob.status.confuse.add.target",
+    "pokemob.status.confuse.remove.target",
+    "pokemob.status.confusion.target",
+    "pokemob.status.infatuate.target",
+    "pokemob.move.stat.fail.target",
+    "pokemob.move.super.effective.user",
+    "pokemob.move.not.very.effective.user",
+    "pokemob.move.doesnt.affect.user",
+    "pokemob.move.critical.hit.user",
+    "pokemob.move.super.effective.target",
+    "pokemob.move.not.very.effective.target",
+    "pokemob.move.doesnt.affect.target",
+    "pokemob.move.critical.hit.target",
+    "pokemob.move.used.user",
+    "pokemob.move.used.target",
+    "pokemob.move.stat.fail.user",
+    "pokemob.move.stat.rise1.user",
+    "pokemob.move.stat.rise2.user",
+    "pokemob.move.stat.rise3.user",
+    "pokemob.move.stat.fall1.user",
+    "pokemob.move.stat.fall2.user",
+    "pokemob.move.stat.fall3.user",
+    "pokemob.move.stat.fail.target",
+    "pokemob.move.stat.rise1.target",
+    "pokemob.move.stat.rise2.target",
+    "pokemob.move.stat.rise3.target",
+    "pokemob.move.stat.fall1.target",
+    "pokemob.move.stat.fall2.target",
+    "pokemob.move.stat.fall3.target",
+    "pokemob.move.stat1",
+    "pokemob.move.stat2",
+    "pokemob.move.stat3",
+    "pokemob.move.stat4",
+    "pokemob.move.stat5",
+    "pokemob.move.stat6",
+    "pokemob.move.stat7",
+    "pokemob.move.notify.learn",
+    "pokemob.move.terraindamage",
+    "pokemob.move.missed.user",
+    "pokemob.move.missed.target",
+    "pokemob.move.failed.user",
+    "pokemob.move.failed.target",
+    "pokemob.move.isfrozen.user",
+    "pokemob.move.issleeping.user",
+    "pokemob.move.isfullyparalyzed.user",
+    "pokemob.move.paralyzed.user",
+    "pokemob.move.isburned.user",
+    "pokemob.move.ispoisoned.user",
+    "pokemob.move.isbadlypoisoned.user",
+    "pokemob.move.isfrozen.target",
+    "pokemob.move.issleeping.target",
+    "pokemob.move.isfullyparalyzed.target",
+    "pokemob.move.paralyzed.target",
+    "pokemob.move.isburned.target",
+    "pokemob.move.ispoisoned.target",
+    "pokemob.move.isbadlypoisoned.target",
+    "pokemob.move.sketched",
+    "pokemob.move.cooldown",
+        ]
+        with open('base_moves.json', 'r') as base_moves_file:
+            base_moves_data = json.load(base_moves_file)
+        for key in ignore_fields:
+            if base_moves_data.get(key):
+                base_moves_data.pop(key)
+        base_moves_data['_comment'] = '#Move Names'
+        with open('base_moves.json', 'w') as base_moves_file:
+            json.dump(base_moves_data, base_moves_file)
+
+    def cleanup_base_files(self):
+        self.archive.close()
+        if self.debug:
+            print(f'Closed {self.archive_path} zipfile handler')
+        for asset_file in self.asset_files:
+            if os.path.exists(asset_file):
+                if self.debug:
+                    print(f'Removed {asset_file}')
+                os.remove(asset_file)
+
+    def extract_base_file(self, path, target):
+        if self.debug:
+            print('Extracting', path, 'to', target)
+        with self.archive.open(path, 'r') as archive_file:
+            with open(target, 'w') as target_file:
+                for line in archive_file.readlines():
+                    target_file.write(line.decode('utf-8'))
+        if target == 'base_moves.json':
+            self.clear_base_move_file()
+
+    def get_archive_version(self):
+        manifest = self.archive.open('META-INF/MANIFEST.MF', 'r') # Implementation-Version: X.X.X
+        for line in manifest:
+            if line.decode('utf-8').split(':')[0].lower() == 'implementation-version':
+                return line.decode('utf-8').split(':')[1].lower().strip()
+            if line.decode('utf-8').split(':')[0].lower() == 'specification-version':
+                return line.decode('utf-8').split(':')[1].lower().strip()
+        return '0.0.0'
--- pokemob-translation-pack-creator.py
+++ pokemob-translation-pack-creator.py
@@ -6,29 +6,48 @@ import sys
 import config
 from lib.scraper import PokeAPIScraper
 from lib.resourcepack import ResourcePack
+from lib.extractor import BaseFileExtractor
 
 
-pokecube_version = '3.7.0'  # TODO: this will be taken from the jar later on
 date = datetime.datetime.now().strftime('%Y-%m-%d')
+pokecube_jar = None
 
 if len(sys.argv) > 1:
     arguments = sys.argv[1:]
     for argument in arguments:
         if '--debug' in argument:
             config.debug = True
-        if '--version' in argument:
-            pokecube_version = argument.split('=')[1]
+        if '--jar' in argument:
+            pokecube_jar = argument.split('=')[1]
 
 scraper = PokeAPIScraper(
     config.api, cache_dir=config.cache_directory, limit=config.limit, debug=config.debug)
 
 scraper.setup()
 if config.debug:
-    print('Scraper setup, done')
+    print('Scraper setup, done\n')
 
 
-# TODO: replace with JAR extractor
-if os.path.exists('base_mobs.json') and os.path.exists('base_moves.json'):
-    for language in config.languages:
-        resource_pack = ResourcePack(language, pokecube_version, date, config.debug)
-        resource_pack.build_pack()
+if pokecube_jar:
+    if os.path.exists(pokecube_jar):
+        extractor = BaseFileExtractor(pokecube_jar, config.debug)
+        pokecube_version = extractor.get_archive_version()
+        if config.debug:
+            print(f'Found pokecube version {pokecube_version}')
+        extractor.extract_base_files()
+        for language in config.languages:
+            resource_pack = ResourcePack(language, pokecube_version, date, config.debug)
+            resource_pack.build_pack()
+        extractor.cleanup_base_files()
+    else:
+        print(f'Could not find {pokecube_jar}!')
+else:
+    print('No jar file specified, please try again and include --jar=your-pokecube.jar')
+
+
+# pokecube_version = '3.7.0'  # TODO: this will be taken from the jar later on
+# # TODO: replace with JAR extractor
+# if os.path.exists('base_mobs.json') and os.path.exists('base_moves.json'):
+#     for language in config.languages:
+#         resource_pack = ResourcePack(language, pokecube_version, date, config.debug)
+#         resource_pack.build_pack()
